278addebbeeec0e5a92ff851aa8045630ed17201
hardikgw <hardikgw@gmail.com>
2017-02-22 16:35:24 -0500

    Update Indexer.java

--- src/main/java/biz/channelit/search/ingest/elastic/Indexer.java
+++ src/main/java/biz/channelit/search/ingest/elastic/Indexer.java
@@ -1,129 +1,66 @@
-package biz.channelit.search.ingest.elastic;
+package biz.channelit.search.ingest.opennlp;
 
-import biz.channelit.search.ingest.corenlp.CoreNlpNer;
-import biz.channelit.search.ingest.opennlp.OpenNlpNer;
-import biz.channelit.search.ingest.tika.Extractor;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.search.SearchRequestBuilder;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.index.query.QueryBuilder;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.util.Span;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import static org.elasticsearch.index.query.QueryBuilders.*;
-
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import java.util.*;
 
 /**
- * Created by hp on 2/14/17.
+ * Created by hp on 2/21/17.
  */
 
 @Service
-public class Indexer {
+public class OpenNlpNer {
 
     @Autowired
-    TransportClient client;
-
-    List<File> files;
-
-    @Value("${crawler.path}")
-    String crawlerpath;
-
-    @Value("${elastic.default.type}")
-    String defaultType;
-
-    @Value("${elastic.default.index}")
-    String defaultIndex;
+    Tokenizer tokenizer;
 
     @Autowired
-    CoreNlpNer coreNlpNer;
+    NameFinderME orgNameFinder;
 
     @Autowired
-    OpenNlpNer openNlpNer;
+    NameFinderME personNameFinder;
 
-    public void indexFiles() {
-        files = new LinkedList<>();
-        walk(crawlerpath);
-        files.forEach(file -> {
-            String content = extractFileContet(file).replaceAll("[^A-Za-z ]", " ").replaceAll(" +", " ");
-//            Map<String, List<String>> map = openNlpNer.getAll(content);
-            try {
-                IndexResponse response = client.prepareIndex(defaultIndex, defaultType)
-                        .setSource(jsonBuilder()
-                                .startObject()
-                                .field("body", content)
-                                .field("insertDate", new Date())
-//                                .map(map)
-                                .endObject()
-                        )
-                        .get();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+    @Autowired
+    NameFinderME locationFinder;
 
-    }
+    @Autowired
+    SentenceDetectorME sdetector;
 
-    private String extractFileContet(File file) {
-        return Extractor.extractFileContet(file);
-    }
+    public List<String> findNer(NameFinderME nameFinder, String[] tokens) {
+        List<String> names = new ArrayList<>();
+        try {
+            Span nameSpans[] = nameFinder.find(tokens);
+            for (Span s : nameSpans)
+                names.add(String.join(" ", Arrays.copyOfRange(tokens, s.getStart(), s.getEnd())));
 
-    private void walk(String path) {
-
-        File root = new File(path);
-        File[] list = root.listFiles();
-
-        if (list == null) return;
-
-        for (File f : list) {
-            if (f.isDirectory()) {
-                walk(f.getAbsolutePath());
-                System.out.println("Dir:" + f.getAbsoluteFile());
-            } else {
-                files.add(f);
-                System.out.println("File:" + f.getAbsoluteFile());
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return names;
     }
 
-    public UpdateResponse update(String index, String type, String id, Map<String, List<String>> map) throws IOException, ExecutionException, InterruptedException {
-        UpdateRequest updateRequest = new UpdateRequest();
-        updateRequest.index(index).type(type);
-        updateRequest.id(id);
-        updateRequest.doc(map);
-        return client.update(updateRequest).get();
-    }
-
-    public SearchResponse search(String index, String type, String query) {
-        QueryBuilder qb = queryStringQuery(query);
-        return client.prepareSearch(index).setTypes(type).setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                .setQuery(qb).setFetchSource("body",null)
-                .setSize(10).execute().actionGet();
-    }
-
-    public void populateNer(String index, String type, String query) {
-        SearchResponse searchResponse = search(index, type, query);
-        searchResponse.getHits().forEach((hit) -> {
-            Map source = hit.getSource();
-            String content = (String) source.get("body");
-            Map<String, List<String>> map = openNlpNer.getAll(content);
-            try {
-                update(index,type, hit.getId(),map);
-            } catch (IOException | ExecutionException | InterruptedException e) {
-                e.printStackTrace();
+    public Map<String, List<String>> getAll(String text) {
+        Map<String, List<String>> out = new HashMap<>();
+        String[] sentences = sdetector.sentDetect(text);
+        try {
+            for (String sentence : sentences) {
+                if (sentence.trim().length() > 40) {
+                    String[] tokens = tokenizer.tokenize(sentence);
+                    out.put("persons", findNer(personNameFinder, tokens));
+                    out.put("companies", findNer(orgNameFinder, tokens));
+                    out.put("locations", findNer(locationFinder, tokens));
+                    out.put("ner", new LinkedList<>());
+                }
             }
-        });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out;
     }
 }
