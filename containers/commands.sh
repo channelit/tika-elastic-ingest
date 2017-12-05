beb7b84aa244da941500db0ebd6f2ed94c673c68
Hardik Patel <hardikgw@gmail.com>
2017-12-05 03:59:06 -0500

    docker compose new version

--- containers/commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#docker run -d --name=es -v $(pwd)/elastic/config:/usr/share/elasticsearch/config -v $(pwd)/elastic/data:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 elasticsearch -Etransport.host=0.0.0.0 -Ediscovery.zen.minimum_master_nodes=1
-docker run -d --name=es -v $(pwd)/elastic/data:/usr/share/elasticsearch/data -v $(pwd)/elastic/logs:/usr/share/elasticsearch/logs -p 9200:9200 -p 9300:9300 elasticsearch -Etransport.host=0.0.0.0 -Ediscovery.zen.minimum_master_nodes=1
-#docker run -d --name=kibana  --link es:localhost -e ELASTICSEARCH_URL=http://0.0.0.0:9200 -p 5601:5601 kibana
-#docker run -d --name=kibana  --link es:elasticsearch -p 5601:5601 kibana
-
-# x-pack installation
-#docker exec es /usr/share/elasticsearch/bin/elasticsearch-plugin install x-pack
-#docker exec kibana /usr/share/kibana/bin/kibana-plugin install x-pack
