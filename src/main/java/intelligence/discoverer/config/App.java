1a0ba3d7c6998a753286c28109053f21b11f04cb
hardikgw <hardikgw@gmail.com>
2017-12-05 13:05:41 -0500

    Update App.java

--- src/main/java/intelligence/discoverer/config/App.java
+++ src/main/java/intelligence/discoverer/config/App.java
@@ -154,5 +154,27 @@ public class App {
                 .build();
         return bulkProcessor;
     }
+    
+    @Bean
+    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpClient);
+
+        return new RestTemplate(requestFactory);
+    }
 
 }
