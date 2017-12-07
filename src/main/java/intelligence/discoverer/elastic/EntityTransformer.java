72431c4eccc764fb423a2da3f0c9b4bf39e3c607
Hardik Patel <hardikgw@gmail.com>
2017-12-07 14:44:32 -0500

    round and round

--- src/main/java/intelligence/discoverer/elastic/EntityTransformer.java
+++ src/main/java/intelligence/discoverer/elastic/EntityTransformer.java
@@ -11,19 +11,11 @@ import java.util.*;
 @Service
 public class EntityTransformer {
 
-    private final String fieldMapFilePath = "src/main/resources/settings/fieldmap.yml";
-
-    private Properties fieldMap;
-
-    public EntityTransformer() throws IOException {
-        fieldMap.load(new FileInputStream(fieldMapFilePath));
-    }
 
     public Map<String,Object> getFieldValues(String json) {
         JsonParser parser = new JsonParser();
         JsonObject o = parser.parse(json).getAsJsonObject();
         Map<String, List<?>> fieldVal = new HashMap<>();
-        fieldMap.keySet().forEach(k-> fieldVal.put(k.toString(), new ArrayList<>()));
         String type = o.get("type").getAsString();
         switch (type){
 
