5f5f47736123c07f157ff28df9e0fb5df7724854
Hardik Patel <hardikgw@gmail.com>
2017-12-07 13:41:36 -0500

    round and round

--- src/main/java/intelligence/discoverer/elastic/EntityTransformer.java
+++ src/main/java/intelligence/discoverer/elastic/EntityTransformer.java
@@ -1,14 +1,33 @@
 package intelligence.discoverer.elastic;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
 
 @Service
 public class EntityTransformer {
 
-    public Map<String,Object> getFieldValues(String resp) {
+    private final String fieldMapFilePath = "src/main/resources/settings/fieldmap.yml";
+
+    private Properties fieldMap;
+
+    public EntityTransformer() throws IOException {
+        fieldMap.load(new FileInputStream(fieldMapFilePath));
+    }
+
+    public Map<String,Object> getFieldValues(String json) {
+        JsonParser parser = new JsonParser();
+        JsonObject o = parser.parse(json).getAsJsonObject();
+        Map<String, List<?>> fieldVal = new HashMap<>();
+        fieldMap.keySet().forEach(k-> fieldVal.put(k.toString(), new ArrayList<>()));
+        String type = o.get("type").getAsString();
+        switch (type){
+
+        }
         return new HashMap<>();
     }
 }
