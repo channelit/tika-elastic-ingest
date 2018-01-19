6978492135c70ece3a494704d29922c49a548520
Hardik Patel <hardikgw@gmail.com>
2018-01-19 14:21:25 -0500

    osm

--- src/main/java/intelligence/discoverer/elastic/FileIndexer.java
+++ src/main/java/intelligence/discoverer/elastic/FileIndexer.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -37,14 +38,22 @@ public class FileIndexer {
     @Autowired
     OpenNlpNer openNlpNer;
 
+    @Value("${file.maxsize}")
+    Long fileMaxSize;
+
     public String indexFile(Path file, BasicFileAttributes attrs) throws IOException {
-        Map<String, Object> map = entityExtractorClient.processFile(file);
+        Map<String, Object> map = new HashMap<>();
         map.put("file_size", attrs.size());
         map.put("file_accessed", attrs.lastAccessTime());
         map.put("file_created", attrs.creationTime());
         map.put("file_url", file.toAbsolutePath().toString());
         map.put("file_modified", attrs.lastModifiedTime());
         map.put("category", "IMP");
+        if (attrs.size() < fileMaxSize) {
+            map.putAll(entityExtractorClient.processFile(file));
+        } else {
+            map.put("error", "large");
+        }
         bulkProcessor.add(new IndexRequest(defaultIndex, defaultType).source(map));
         return "ok";
     }
